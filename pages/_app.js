8844b53e50ee98ad391983c46ed249c3c118ce34
samuelghill <samuelgarretthill@gmail.com>
2021-04-29 21:59:48 -0500

    updated endpoint

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
 	headers: {
-		Authorization: `Apikey ${process.env.STEPZEN_ADMIN_KEY}`,
+		Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_ADMIN_KEY}`,
 	},
 	uri: `https://${process.env.NEXT_PUBLIC_STEPZEN_ACCOUNT}.stepzen.net/${process.env.NEXT_PUBLIC_STEPZEN_FOLDER}/${process.env.NEXT_PUBLIC_STEPZEN_NAME}/__graphql`,
 });
